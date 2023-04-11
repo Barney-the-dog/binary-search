f858b5654f02ce4c2fbd19e621c34f94d789b39b
Irina Ashurova <you-ve_got@mail.ru>
2023-04-11 17:36:40 -0400

    done

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 // Этот метод должен искать элемент в массиве и возвращать его индекс или -1, если элемент не находится в массиве.
 // Например, если ты ищешь G в массиве [A, B, C, D, E, F, G, H], твой метод должен возвращать 6. Если ты ищешь X, он должен возвращать -1.
 
-function binarySearch (){
-
+function binarySearch (n, arr, start, end){
+    let mid = Math.floor((start + end)/2)
+    if (start > end) {
+        return -1;
+      } 
+    if (n==arr[mid]) {
+        return mid
+    }
+    if (n < arr[mid]){
+        return binarySearch (n, arr, start, mid-1);
+    } 
+    if (n > arr[mid]){
+        return binarySearch(n, arr, mid+1, end )
+   }
 }
+
+
+// function binarySearch (n, arr){
+//     let start = 0
+//     let end = arr.length - 1
+//    while (start <= end) {
+//     let mid = Math.floor((start + end)/2)
+//     if (n==arr[mid]) {
+//       return mid
+//     } else if (n < arr[mid]){
+//       end = mid - 1
+//     } else if (n > arr[mid]){
+//       start = mid + 1
+//     } 
+//    }
+//     return -1
+//   }
+  console.log(binarySearch(7, [1, 2, 3, 4, 5, 6, 7, 8]))
 
 module.exports = binarySearch
--- spec/binarysearch.spec.js
+++ spec/binarysearch.spec.js
@@ -1,13 +1,25 @@
 const binarySearch = require('../index')
 
 test('Функция должна возвращать индекс элемента', () => {
+    describe("binarySearch()", function() {
+        it("returns first index of item in array", function() {
+          let item = 3;
+          let arr = [1,2,3,4,5];
+          expect(binarySearch(item, arr)).toEqual(2);
+        });
+        });
     
 })
 
-test('Функция должна быть определена', () => {
-    
-})
+test('Should be defined', () => {
+    expect(binarySearch(item, arr)).toBeDefined();
+      });
+      
 
 test('Функция должна возвращать -1, если элемента не существует в данном массиве', () => {
-    
+    it("returns -1 if not in array", function() {
+        let item = 6;
+        let arr = [1,2,3,4,5];
+        expect(binarySearch(item, arr)).toEqual(-1);
+      });   
 })
